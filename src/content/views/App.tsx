88598c8b80291b4c17fba47319e9166e39f4aa91
Devesh Chandra <deveshru2712@gmail.com>
2025-10-02 14:14:19 +0530

    made some small changes in the ui

---
+++ src/content/views/App.tsx
@@ -0,0 +1,25 @@
+import Logo from "@/assets/crx.svg";
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [show, setShow] = useState(false);
+  const toggle = () => setShow(!show);
+
+  return (
+    <div className="popup-container">
+      {show && (
+        <div
+          className={`popup-content ${show ? "opacity-100" : "opacity-0"} bg-green-300`}
+        >
+          <h1>HELLO CRXJS</h1>
+        </div>
+      )}
+      <button className="toggle-button" onClick={toggle}>
+        <img src={Logo} alt="CRXJS logo" className="button-icon" />
+      </button>
+    </div>
+  );
+}
+
+export default App;
--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -2,7 +2,7 @@ import Overlay from "@/components/Overlay";
 
 export default function App() {
   return (
-    <div className="relative aspect-square w-[320px] rounded-2xl border">
+    <div className="aspect-square w-[310px]">
       {/* Dashed Grid */}
       <Overlay />
       {/* content */}
@@ -15,7 +15,7 @@ export default function App() {
         {/* button with aria-describedby for accessibility */}
         <button
           type="button"
-          className="bg-primary text-primary-foreground focus-visible:ring-ring inline-flex items-center justify-center rounded-md px-4 py-2 text-sm font-medium shadow-sm hover:opacity-90 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none"
+          className="bg-primary text-primary-foreground focus-visible:ring-ring inline-flex cursor-pointer items-center justify-center rounded-md px-4 py-2 text-sm font-medium shadow-sm hover:opacity-90 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none"
           aria-label="Block this website"
           aria-describedby="block-desc"
         >
